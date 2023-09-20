496913d6a07b79044f7a10e2ccf40c19a7cec749
twanluttik <twanluttik@gmail.com>
2023-09-19 21:51:57 -0700

    moving to a new domain

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,4 +3,4 @@ export * from './core';
 export * from './helper';
 
 export const OLLAMA_HOST = `127.0.0.1:11435`;
-export const OLLAMA_COMMAND = `OLLAMA_ORIGINS=https://ollama-web-ui.vercel.app OLLAMA_HOST=${OLLAMA_HOST} ollama serve`;
+export const OLLAMA_COMMAND = `OLLAMA_ORIGINS=https://ollama.twanluttik.com OLLAMA_HOST=${OLLAMA_HOST} ollama serve`;
