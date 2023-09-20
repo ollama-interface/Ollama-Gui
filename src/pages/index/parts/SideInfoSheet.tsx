d93398e2ab3f1e2588653188cc2214ce5ea89f45
twanluttik <twanluttik@gmail.com>
2023-09-19 19:51:49 -0700

    added github repo link

--- src/pages/index/parts/SideInfoSheet.tsx
+++ src/pages/index/parts/SideInfoSheet.tsx
@@ -34,58 +34,68 @@ export const SideInfoSheet: React.FC<ISideInfoSheetProps> = ({ loading }) => {
             Thank you for visiting this website, I made this because there is no
             web chat interface I have found at the time building this.
           </SheetDescription>
-          <div className="h-100%">
-            <Label className="mb-1 font-medium text-neutral-900 mr-1">
-              Ollama:
+        </SheetHeader>
+
+        <div className="h-100%">
+          <Label className="mb-1 font-medium text-neutral-900 mr-1">
+            Ollama:
+          </Label>
+          <a
+            href="https://ollama.ai/"
+            className="text-sm  underline underline-offset-4"
+          >
+            https://ollama.ai/
+          </a>
+          <div className="flex flex-col mt-4">
+            <Label className="mb-1 font-medium text-neutral-900">
+              Ollama remote address:
             </Label>
-            <a
-              href="https://ollama.ai/"
-              className="text-sm  underline underline-offset-4"
-            >
-              https://ollama.ai/
-            </a>
-            <div className="flex flex-col mt-4">
-              <Label className="mb-1 font-medium text-neutral-900">
-                Ollama remote address:
-              </Label>
-              <Input
-                disabled={loading}
-                type="text"
-                placeholder="Ollama url"
-                value={url}
-                onChange={(e) => core.localAPI.set(e.currentTarget.value)}
-              />
-              <Label className="mt-6 mb-1 font-medium text-neutral-900">
-                Serve command for ollama:
-              </Label>
-              <code className="relative rounded bg-neutral-200 px-[0.5rem] py-[0.5rem] font-mono text-sm font-semibold pb-8">
-                {OLLAMA_COMMAND}
-              </code>
-              <div className="flex justify-end mt-2">
-                <Button
-                  onClick={() => {
-                    navigator.clipboard.writeText(OLLAMA_COMMAND);
-                  }}
-                >
-                  <ClipboardCopyIcon className="mr-2" />
-                  Copy
-                </Button>
-              </div>
-              <a className="italic text-sm text-neutral-800 mt-2">
-                We need to run this, otherwise the website can't access your
-                ollama server that is running on <strong>your</strong> machine.
-              </a>
+            <Input
+              disabled={loading}
+              type="text"
+              placeholder="Ollama url"
+              value={url}
+              onChange={(e) => core.localAPI.set(e.currentTarget.value)}
+            />
+            <Label className="mt-6 mb-1 font-medium text-neutral-900">
+              Serve command for ollama:
+            </Label>
+            <code className="relative rounded bg-neutral-200 px-[0.5rem] py-[0.5rem] font-mono text-sm font-semibold pb-8">
+              {OLLAMA_COMMAND}
+            </code>
+            <div className="flex justify-end mt-2">
+              <Button
+                onClick={() => {
+                  navigator.clipboard.writeText(OLLAMA_COMMAND);
+                }}
+              >
+                <ClipboardCopyIcon className="mr-2" />
+                Copy
+              </Button>
             </div>
+            <a className="italic text-sm text-neutral-800 mt-2">
+              We need to run this, otherwise the website can't access your
+              ollama server that is running on <strong>your</strong> machine.
+            </a>
           </div>
-          <SheetFooter className="pt-20 flex flex-col">
+          <div
+            style={{ height: 'calc(100% - 455px)' }}
+            className="flex flex-col justify-end"
+          >
             <a
               href="https://twitter.com/twanluttik"
-              className="font-medium  underline  text-sm"
+              className="mt-10 text-sm font-semibold hover:opacity-60 tracking-tight"
             >
               Twan Luttik - X (Twitter)
             </a>
-          </SheetFooter>
-        </SheetHeader>
+            <a
+              href="https://github.com/TwanLuttik/ollama-web-ui"
+              className="mt-2 text-sm underline hover:opacity-60"
+            >
+              https://github.com/TwanLuttik/ollama-web-ui
+            </a>
+          </div>
+        </div>
       </SheetContent>
     </Sheet>
   );
