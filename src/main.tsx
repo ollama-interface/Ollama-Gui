990387902d0120453607562e40a148f8f54cd674
twanluttik <twanluttik@gmail.com>
2023-09-20 12:37:08 -0700

    make light default

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
     <TooltipProvider>
       <BrowserRouter>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
           <MainRouter />
         </ThemeProvider>
       </BrowserRouter>
